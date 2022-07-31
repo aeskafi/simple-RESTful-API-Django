358305c5eba27b1fdd187a2583311cda939f9d91
Parisa Khaleghi <parisakhaleghi@Parisas-MacBook-Pro.local>
2022-07-31 22:18:30 +0300

    Fix: change app name from app to myapp

--- python_api/myapp/views.py
+++ python_api/myapp/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
-from app.models import Car
+from myapp.models import Car
 
 def index(request):
     response = json.dumps([{}])
